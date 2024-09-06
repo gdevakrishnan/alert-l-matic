16db635175b6b6924241d1e11efe6150be4d3151
gdevakrishnan <gdevakrishnan700@gmail.com>
2024-09-06 23:02:02 +0530

    Create Static Files for styling

---
+++ alert-l-matic/static/style.js
@@ -0,0 +1,9 @@
+export const style = {
+    // Basic Container
+    container: {
+        flex: 1,
+        backgroundColor: 'ivory',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+}
